6ff32886526cf224bd9cce8900c37fbf0db9ce6d
Nika <nikam2341@gmail.com>
2021-03-15 23:24:30 +0500

    init: basic-js task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+	const massiv = matrix.reduce((item, amount) => {
+		return item.concat(amount);
+	}, []);
+
+	if (!massiv || massiv.length  == 0 || !massiv.includes('^^')) { return 0;}
+
+	let allcount = massiv.reduce((cat, total) => {
+		cat[total] = (cat[total] || 0) + 1;
+		return cat;
+	}, {})
+
+	return allcount['^^'];
 };
